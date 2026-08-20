8e2cacd8554b0825a584f3d7a71cc49863b62589
Kai-Jennings <149123213+Kai-Jennings@users.noreply.github.com>
2026-08-20 19:01:53 +1000

    Update main.py
    refactor

--- main.py
+++ main.py
@@ -1,57 +1,27 @@
-import re
-from ast import Parser, Flattener
-
-with open("rules.txt", "r", encoding="utf-8") as f:
-    ebnf = f.read()
+from ast import Lexer, Parser, Flattener
 
 
-class Token:
-    def __init__(self, _type, _value):
-        self.type = _type
-        self.value = _value
+def run():
+    with open("rules.txt", "r", encoding="utf-8") as f:
+        ebnf = f.read()
+
+    lexer = Lexer(ebnf)
+    token_stream = lexer.tokenise()
+    print("Token Stream Generated")
+
+    parser = Parser(token_stream)
+    ast = parser.parse_grammar()
+    print("AST Generated")
+
+    flattener = Flattener(ast)
+    flat_ast = flattener.flatten_grammar()
+    print("AST Flattened")
+
+    print(flat_ast)
 
 
-def tokenise(ebnf_str):
-    tokens = []
-    pointer = 0
-
-    identifier_re = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*')
-    terminal_re = re.compile(r'"[^"]*"')
-    operators = {'=', ',', '|', '{', '}', '[', ']', ';'}
-
-    while pointer < len(ebnf_str):
-
-        char = ebnf_str[pointer]
-
-        if char.isspace():
-            pointer += 1
-            continue
-
-        if char in operators:
-            tokens.append(Token("OPERATOR", char))
-            pointer += 1
-            continue
-
-        terminal_match = terminal_re.match(ebnf_str, pointer)
-        if terminal_match:
-            tokens.append(Token("TERMINAL", terminal_match.group(0)))
-            pointer = terminal_match.end()
-            continue
-
-        identifier_match = identifier_re.match(ebnf_str, pointer)
-        if identifier_match:
-            tokens.append(Token("IDENTIFIER", identifier_match.group(0)))
-            pointer = identifier_match.end()
-            continue
-
-    return tokens
-
-
-# print("\n".join(f"{x.type:<12}{x.value:<10}" for x in tokenise(ebnf)))
-tokens = tokenise(ebnf)
-parser = Parser(tokens)
-flattener = Flattener()
-print(flattener.flatten_grammar(parser.parse_grammar()))
+if __name__ == "__main__":
+    run()
 
 
 """
