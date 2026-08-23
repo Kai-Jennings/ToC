7571434c9e31d5e57ff6163a123816cbfda0cd21
Kai-Jennings <149123213+Kai-Jennings@users.noreply.github.com>
2026-08-23 18:07:38 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,8 @@
-from language import Lexer, Parser, Flattener, FirstSet, FollowSet, ParseTable, LL1Parser
-
-
-def run():
-    with open("rules.txt", "r", encoding="utf-8") as f:
-        ebnf = f.read()
-
-    lexer = Lexer(ebnf)
-    token_stream = lexer.tokenise()
-    print("Token Stream Generated")
-
-    parser = Parser(token_stream)
-    ast = parser.parse_grammar()
-    print("AST Generated")
-
-    flattener = Flattener(ast)
-    flat_ast = flattener.flatten_grammar()
-    print("AST Flattened")
-
-    first_calc = FirstSet(flat_ast)
-    first_sets = first_calc.calculate()
-    print("First Sets Generated")
-
-    follow_calc = FollowSet(flat_ast, first_calc, "input")
-    follow_sets = follow_calc.calculate()
-    print("Follow Sets Generated")
-
-    table_gen = ParseTable(flat_ast, first_calc, follow_sets)
-    ll1_table = table_gen.generate()
-    print("Generated LL(1) Parse Table")
-
-    ll1_parser = LL1Parser(ll1_table, debug=True)
-    print(ll1_parser.parse(list("λx.λy.λz.xyz"), "input"))
-
+from language import Engine
 
 if __name__ == "__main__":
-    run()
+    engine = Engine("input")
+    print(engine.parse(list("λx.λy.λz.xyz"), debug=True))
 
 """
 input = { " " } , expression , { " " } ;
